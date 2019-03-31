2c2abb1e8832a591ffa1cb5d6dd1600dbf13ccdf
futoukeji <marrysix@163.com>
2019-03-31 18:40:36 +0800

    121

--- index.php
+++ index.php
@@ -2,18 +2,18 @@
 // +----------------------------------------------------------------------
 // | OPO [ SOA ]
 // +----------------------------------------------------------------------
-// | Copyright (c) 2006-2016 http://bfw.life All rights reserved.
+// | Copyright (c) 2006-2018 http://bfw.life All rights reserved.
 // +----------------------------------------------------------------------
 // | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
 // +----------------------------------------------------------------------
 // | Author: wangbo <opo@gmail.com>
 // +----------------------------------------------------------------------
 // [ 应用入口文件 ]
-if (version_compare ( "5.4", PHP_VERSION, ">" )) {
-	die ( "PHP 5.4 or greater is required!!!" );
+if (version_compare("5.4", PHP_VERSION, ">")) {
+    die("PHP 5.4 or greater is required!!!");
 }
-define ( "APP_BASE", __DIR__ );
-define ( "DS", DIRECTORY_SEPARATOR );
-define ( "APP_ROOT", substr(APP_BASE, 0,strripos(APP_BASE,DS)). DS . "Bfw" );
-define ( "APP_DIR", APP_ROOT. DS . "App" .DS);
-require_once APP_ROOT . DS . 'Lib' . DS . 'Init.php';
+define("APP_BASE", __DIR__);
+define("DS", DIRECTORY_SEPARATOR);
+define("APP_ROOT", substr(APP_BASE, 0, strripos(APP_BASE, DS)) . DS . "Bfw");
+define("BFW_LIB", APP_ROOT);
+require_once BFW_LIB .DS."Lib". DS . 'Init.php';
