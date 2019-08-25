ca462665dd1ff2d3fc66b87accdf926ce05ae1d3
wangbo <425876395@qq.com>
2019-08-25 17:33:26 +0800

    Signed-off-by: wangbo <425876395@qq.com>

--- Bfw/Lib/BoRoute.php
+++ Bfw/Lib/BoRoute.php
@@ -29,7 +29,10 @@ class BoRoute
                 $_pathurl=$_SERVER['QUERY_STRING'];
             }
         }
-       // if($_pathurl!=""){
+        if($_pathurl==""){
+            $_pathurl="/";
+        }
+        if($_pathurl!=""){
             $_routedata = &Registry::getInstance()->get("route_data");
             if (! is_null($_routedata)) {
                foreach ( $_routedata as $_iurl => $_furl){
@@ -58,7 +61,7 @@ class BoRoute
                        }
                    }
                }
-           // }
+            }
             if (PAGE_SUFFIX != "") {
                 $_pathurl=str_replace(PAGE_SUFFIX,"",$_pathurl);
             }
